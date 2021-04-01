b985cef39ae2bc3f916f50db417773d0fee91d70
KirychMe <81570537+KirychMe@users.noreply.github.com>
2021-04-01 14:08:46 +0300

    Add files via upload

---
+++ Cat.py
@@ -0,0 +1,11 @@
+from Test import Cat
+
+c1 = Cat('Baron', 'Male', '2 years')
+c2 = Cat('Sam', 'Male', '2 years')
+
+
+print("Name =", c1.name + ', ' + c2.name)
+print("Gender =", c1.gender  + ', ' +  c2.gender)
+print("Age =", c1.age  + ', ' +  c2.age)
+
+
