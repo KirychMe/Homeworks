a28d0e644e51dfd2852a3b8792336625409a3f3a
KirychMe <81570537+KirychMe@users.noreply.github.com>
2021-04-01 16:06:03 +0300

    Add files via upload

--- Test_2_1.py
+++ Test_2_1.py
@@ -8,3 +8,6 @@ class Rectangle:
     def get_rectanle(self):
         return str(self.x), str(self.y), str(self.width), str(self.height)
 
+    def get_area(self):
+        return self.x*self.y
+
--- rectangle_2_1.py
+++ rectangle_2_1.py
@@ -1,8 +1,8 @@
 from Test_2_1 import Rectangle
 
-#далее создаем два прямоугольника
 
 rect_1 = Rectangle(5, 10, 15, 20)
-#rect_2 = Rectangle(12,5)
-#вывод площади наших двух прямоугольников
 print(rect_1.get_rectanle())
+
+rect_2 = Rectangle(5, 10, 0, 0)
+print(rect_2.get_area())
